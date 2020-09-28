3feeaea8198ca2733cda8e81f72f04714ebf74f2
Sangoro053 <71921060+Sangoro053@users.noreply.github.com>
2020-09-28 17:13:54 +0900

    Update call.py

--- q_search/call.py
+++ q_search/call.py
@@ -5,15 +5,15 @@ from qiimetophitsearch import qiimetophitsearch
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('arg1', help='input file')
-    parser.add_argument('--module', choices=['tophit', 'tophit_all'], help='module name')
+    parser.add_argument('--module', choices=['tophit', 'tophit_all'], required=True, help='module name')
     parser.add_argument('--scpname')
     args = parser.parse_args()
 
-    if args.arg2 == 'tophit':
+    if args.module == 'tophit':
         x = qiimetophitsearch(args.arg1)
         x.tophit(args.scpname)
     
-    elif args.arg2 == 'tophit_all':
+    elif args.module == 'tophit_all':
         x = qiimetophitsearch(args.arg1)
         x.tophit_all()
 
