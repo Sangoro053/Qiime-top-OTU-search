6176bd281a944198032ac16e6c6f4937cfec0759
Sangoro053 <71921060+Sangoro053@users.noreply.github.com>
2020-09-28 11:28:28 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+ 
+setuptools.setup(
+    name="qiimetophitsearch",
+    version="0.1.0",
+    author="midorikawa",
+    author_email="ccm.penaei@gmail.com",
+    description="qiimetophitsearch is my own python package",
+    long_description="hogehoge",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sangoro053?tab=repositories",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]#,
+    #entry_points = {
+    #    'console_scripts': ['init = mycommand.qiimetophitsearch:main']
+    #}
+)
