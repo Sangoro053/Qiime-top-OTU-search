5055f37ef71359fda29a54113a443fc566819693
Sangoro053 <71921060+Sangoro053@users.noreply.github.com>
2020-09-28 15:11:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     entry_points = {
-        'console_scripts': ['q_search = q_search.call:main']
+        'console_scripts': ['q_search=q_search.call:main']
     }
 )
