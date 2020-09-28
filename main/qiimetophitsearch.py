a2adbe5e78fff3d3d518fe1fdf814f78633cf09f
Sangoro053 <71921060+Sangoro053@users.noreply.github.com>
2020-09-28 11:22:33 +0900

    Add files via upload

---
+++ main/qiimetophitsearch.py
@@ -0,0 +1,20 @@
+class qiimetophitsearch:
+    def __init__(self, filename):
+        import pandas as pd
+        self.filename = filename
+
+    def tophit(self, spcname):
+        import pandas as pd
+        df = pd.read_csv(self.filename, delimiter='\t', header=1, index_col=0)
+        return(spcname + ',' + df[spcname].idxmax())
+
+    def tophit_all(self):
+        import pandas as pd
+        maxtaxa=[]
+        df = pd.read_csv(self.filename, delimiter='\t', header=1, index_col=0)
+        df=df.T
+        for i in range(len(df)):
+            maxtaxa.append(df.iloc[i].idxmax())
+        df_new = pd.DataFrame(maxtaxa, index=df.index, columns=['#OTU ID'])
+        return(df_new)
+   
