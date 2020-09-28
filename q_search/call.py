20d8e73996ddfaca2376ed41cdfa8f8bf1ef9202
Sangoro053 <71921060+Sangoro053@users.noreply.github.com>
2020-09-28 14:58:38 +0900

    Add files via upload

---
+++ q_search/call.py
@@ -0,0 +1,19 @@
+import argparse
+
+from qiimetophitsearch import qiimetophitsearch
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('arg1', help='input file')
+    parser.add_argument('--module', choices=['tophit', 'tophit_all'], required=True, help='module name')
+    parser.add_argument('--scpname')
+    args = parser.parse_args()
+
+    if args.arg2 == 'tophit':
+        x = qiimetophitsearch(args.arg1)
+        x.tophit(args.scpname)
+    
+    elif args.arg2 == 'tophit_all':
+        x = qiimetophitsearch(args.arg1)
+        x.tophit_all()
+
