b7875d45b8253afe0630ae0fcb2bfd9c5b448e86
Sangoro053 <71921060+Sangoro053@users.noreply.github.com>
2020-09-28 18:19:50 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
  
 setuptools.setup(
     name="main",
-    version="0.1.5",
+    version="0.1.6",
     author="midorikawa",
     author_email="ccm.penaei@gmail.com",
     description="qiimetophitsearch is my own python package",
