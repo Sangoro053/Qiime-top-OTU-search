8fddd06693bab84b2ba2b2b744e02b0a67c112c4
Sangoro053 <71921060+Sangoro053@users.noreply.github.com>
2020-10-03 15:09:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ import setuptools
  
 setuptools.setup(
     name="Qiime-top-OTU-search",
-    version="0.1.7",
+    version="0.1.8",
     author="midorikawa",
     author_email="ccm.penaei@gmail.com",
     description="qiimetophitsearch is my own python package",
     long_description="hogehoge",
     long_description_content_type="text/markdown",
     url="https://github.com/Sangoro053?tab=repositories",
+    install_requires=['pandas'],
     packages=['q_search', 'qiimetophitsearch'],
     classifiers=[
         "Programming Language :: Python :: 3.7.6",
