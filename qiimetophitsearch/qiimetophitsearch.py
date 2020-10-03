234728f0e71329dbea254314ab2108dc1f346638
Sangoro053 <71921060+Sangoro053@users.noreply.github.com>
2020-10-03 17:31:04 +0900

    Update qiimetophitsearch.py

--- qiimetophitsearch/qiimetophitsearch.py
+++ qiimetophitsearch/qiimetophitsearch.py
@@ -6,7 +6,7 @@ class qiimetophitsearch:
     def tophit(self, spcname):
         #import pandas as pd
         df = pd.read_csv(self.filename, delimiter='\t', header=1, index_col=0)
-        return(spcname + ',' + df[spcname].idxmax())
+        return(spcname + '\t' + df[spcname].idxmax())
 
     def tophit_all(self):
         #import pandas as pd
@@ -16,5 +16,6 @@ class qiimetophitsearch:
         for i in range(len(df)):
             maxtaxa.append(df.iloc[i].idxmax())
         df_new = pd.DataFrame(maxtaxa, index=df.index, columns=['#OTU ID'])
-        return(df_new)
+        df_new_tsv = df_new.to_csv('output.tsv', sep='\t', index=True)
+        return(df_new_tsv)
    
